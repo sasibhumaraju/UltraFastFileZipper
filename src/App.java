a8d93256976f05f3395471cea6c49280e0e4d705
Bhumaraju <s.bhumaraju@dxc.com>
2025-01-10 00:11:23 +0530

    bugs fixed

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
    
     public static void main(String[] args) throws Exception {
         Message.info("Welcome to UltraZipper", 0);
+        Scanner sc = new Scanner(System.in);
 
         while(true) {
             Message.info("Please Select Options (1-4)", 1);
@@ -11,19 +12,20 @@ public class App {
             Message.info("2) File UnZipper", 0);
             Message.info("3) List All Zipped Files", 0);
             Message.info("4) Exit", 1);
-            Scanner sc = new Scanner(System.in);
-            try {
-                int i = sc.nextInt();
-                if(i==4) break;
-                switch (i) {
-                    case 1: FileZipper.compress(); break;
-                    case 2: FileZipper.decompress(); break;
-                    case 3: FileZipper.showAllZippedFiles(); break;
-                    default: Message.error("Please provide valid input", 1);
+            
+                try {
+                    int i = sc.nextInt();
+                    if(i==4) break;
+                    switch (i) {
+                        case 1: FileZipper.compress(); break;
+                        case 2: FileZipper.decompress(); break;
+                        case 3: FileZipper.showAllZippedFiles(); break;
+                        default: Message.error("Please provide valid input", 1);
+                    }
+                } catch (Exception e) {
+                    sc.nextLine();
+                    Message.error("Please provide input in numbers (1-4)..", 1);
                 }
-            } catch (Exception e) {
-                Message.error("Please provide input in numbers only (1-4)", 1);
-            }
         }
         sc.close();
         Message.info("Thank you. Have a Good Day", 2);
